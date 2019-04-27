b71c52e17103028556f5ce21887da8b5dc314104
DragonKoko <koko2405000@gmail.com>
2019-04-27 18:37:04 +0200

    change port to default

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(express.static('public'));
 var bodyParser = require('body-parser')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
-const port = 3000;
+const port = 80;
 
 
 
