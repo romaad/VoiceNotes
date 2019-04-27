d389da1ec2ef2fac0ea49569d3fa8a570f5fd6f6
DragonKoko <koko2405000@gmail.com>
2019-04-28 01:20:04 +0200

    driver listeners count

--- public/js/voice_notes.js
+++ public/js/voice_notes.js
@@ -1,11 +1,14 @@
-var person = "";
+/*file to be used to test user(passenger) side*/
+/*user journey and trip ids and last retrieved voice note time*/
 var  info = {journey_id: -1, trip_id: -1, lastTime: 0};
 $(() => {
+	/*when user issues subscribe to a journey*/
   $("#send").click(()=>{
      info.trip_id =$("#trip_id").val();
      info.journey_id =$("#journey_id").val();
      subscribe();
   });
+  /*when user finishes their ride*/
   $("#finish").click(()=>{
   	$.post('http://localhost:3000/finishTrip', info, (data)=>{
   			console.log(data);
@@ -16,24 +19,32 @@ $(() => {
   	});
   })
 })
+/*add new voice note to messages div */
 function addNotes(note){
 		console.log(JSON.stringify(note));
+		//ignore already received notes
 		if(note.time <= info.time) return;
+		/*use built in html5 audio player to play sounds*/
 		newElem = `<div>
 	   	${new Date(note.time).toLocaleString()}
 	   	<audio id="${note.path}"" controls>
 			  <source src="${note.path}" type="audio/wav">
 				Your browser does not support the audio element.
 			</audio>
+			${note.users.length}
 			</div>`;
 	   $("#messages").append(newElem);
+	   /*update last received timestamp*/
 	   if(note.time > info.lastTime){
 	   	info.lastTime = note.time;
 	   }
 }
+/*get voice notes for current user and add them to view*/
 function getNotes(){
+	/*issue request to server to get most recent records*/
   $.get('http://localhost:3000/getRecords',info, (data) => {
    	console.log(JSON.stringify(data));
+   	/*add each note to message dic*/
    	data.forEach(addNotes);
    	console.log(info);
    }).fail((err)=>{
@@ -41,17 +52,20 @@ function getNotes(){
    })
  }
 function subscribe(){
+	/*subscribe to journey by issuing request*/
 	console.log(info);
    $.post('http://localhost:3000/subscribe', info, (ret)=>{
 			// console.log(JSON.stringify(ret));
 			//if success get notes
 			getNotes();
-			// socket.close();
+			/*remove any old listener to journey*/
 			socket.removeAllListeners(info.journey_id);
+			/*listen to this journey new records updates*/
 			socket.on(info.journey_id, getNotes)
    }).fail((err)=>{
    	alert(err);
    });
 }
+/*socket listener */
 var socket = io();
 	
--- server.js
+++ server.js
@@ -1,68 +1,90 @@
 /* Setting things up. */
-const path = require('path'),
-express = require('express'),
+/*create express object*/
+const express = require('express'),
+/*initialize web server*/
 app = express(),   
+/*setup http server*/
 http = require('http').Server(app),
+/*initialize io socket for real-time notes updates*/
 io = require('socket.io')(http),
+/*use mongoose with mlab as a database*/
 mongoose = require('mongoose'),
+/*use multer for multi-part(files) in requests*/
 multer  = require('multer'), //use multer to upload blob data
+/*make instance of multer*/
 upload = multer(),
+/*use filesystem to save voice notes*/
 fs = require('fs'),
+/*use winston for logging*/
 winston = require('winston'),
+/*make logger instance*/
 logger = new winston.createLogger({
   level: 'info',
   transports: [
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({filename: 'combined.log'})
+    new winston.transports.File({ filename: 'combined.log', level: 'info' }),
+    new winston.transports.File({filename: 'error.log', level: 'error'})
   ]
 }),
+/*path to which server should send to user when providing autio files*/
 uploadPath = '/uploads/'
+/*path to which we save files locally*/
 savePath = __dirname + '/public/uploads/',
+/*mongodb path*/
 db_path = 'mongodb://testUser:tu123123@ds127624.mlab.com:27624/messagesdb';
+/*start mongoose client*/
 mongoose.connect(db_path, {useNewUrlParser: true}, (err) => {
 	logger.log('info', 'mongo working');
 });
-var Message = mongoose.model('Message',{ name : String, message : String});
+/*initialize mongoose subscription model*/
 var Subscription = mongoose.model('Subscription',{ journey_id : String, trip_id : String, is_active: Boolean, time: Number });
-var Recording = mongoose.model('Recording', {journey_id: String, time: Number});
+/*initialize records model*/
+var Recording = mongoose.model('Recording', {journey_id: String, time: Number, users: Array});
+/*set public files to users*/
 app.use(express.static('public'));
+/*use body parser to get request information in req.body*/
 var bodyParser = require('body-parser')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
+/*if port is set by environment use it, else set to 3000*/
 const port = process.env.PORT || 3000;
 
 
-
+/*on root path, send subscription page*/
 app.all('/', function(req, res){
-	res.send("this's the main page hi!");
+	res.sendfile('subscribe.html', { root: __dirname + "/public/subscribe.html" } );
 });
+/*when connecting a user, initialize a web socket*/
 io.on('connection', () =>{
  logger.log('info','a user is connected');
 });
-app.get('/messages', (req, res) => {
-  Message.find({},(err, messages)=> {
-    res.send(messages);
-  });
-});
-
+/*
+	subscribe API, user sends their journey and trip ids in body. This allows user to get records recorded after 
+	this subscription by the specified journey id.
+*/
 app.post('/subscribe/', (req, res) => {
-	/*add user to subscribers to this ride*/
+	/*find trip id in subscribers to this journey*/
 	Subscription.find({journey_id: req.body.journey_id, trip_id: req.body.trip_id, is_active: true}, (err, docLis)=>{
 		if(docLis.length == 1){
 			/*already subscribed user do nothing*/
 			res.status(200).send('user already subscribed');
 		} else if (docLis.length > 1){
-			/*same user subscribed more than once, error*/
-			error = 'same user subscribed twice: ' + docLis[0].journey_id +' ' +docLis[0].trip_id;
+			/*same user subscribed more than once, error, this shouldn't happen*/
+			error = 'same user subscribed more than once: ' + docLis[0].journey_id +' ' +docLis[0].trip_id;
 			logger.error( error);
+			/*send server error, this shouldn't happen*/
 			res.status(500).send(error);
 		}else{
-			var newSub = new Subscription({journey_id: req.body.journey_id, trip_id: req.body.trip_id, is_active: true, time: new Date().getTime()});
+			/*make a new subscription for this trip id and journey id*/
+			var newSub = new Subscription({journey_id: req.body.journey_id,
+			 trip_id: req.body.trip_id, is_active: true, time: new Date().getTime()});
+			/*save this subscription to server*/
 			newSub.save((err) => {
 				if(err){
 					logger.error(err);
+					/*if save failed, send error*/
 					res.status(500).send(JSON.stringify(err));
 				}else{
+					/*if save succeeded, send success code*/
 					logger.log('info', 'subscription made: ' + req.body.journey_id +' ' +req.body.trip_id);	
 					res.sendStatus(200);
 				}
@@ -71,64 +93,101 @@ app.post('/subscribe/', (req, res) => {
 		}
 	});
 });
+/*convert record for a journey at specified time to a file name for record*/
 function getFilePath(journey_id, saveDate){
 	return journey_id + '_' + saveDate + '.wav';
 }
 
+/*alert children of a journey new record for this journey*/
 function updateChildren(jid){
 	//console.log(jid);
 	io.emit(jid);
 }
-
+/*API post request for uploading a record for specified journey, sound file is sent as wav in request body 
+represented by 'soundBlob' key*/
 app.post('/upload/:journey_id', upload.single('soundBlob'), function(req, res, next){
+	/*make a unique timestamp for the request*/
 	let saveDate = new Date().getTime();
+	/*generate file name*/
 	let fileName = getFilePath(req.params.journey_id, saveDate);
+  /*append file name to local path to make final path*/
   let uploadLocation = savePath + fileName;
+  /*write file to disk*/
+  /*this part can be exchanged with remote storage service, such as AWS*/
 	fs.writeFileSync(uploadLocation, Buffer.from(new Uint8Array(req.file.buffer))); // write the blob to the server as a file
-  let recordEntry = new Recording({journey_id: req.params.journey_id, time: saveDate});
+  /*make a db entry for this record*/
+  let recordEntry = new Recording({journey_id: req.params.journey_id, time: saveDate, users: []});
   recordEntry.save((err)=>{
   	if(err){
   		logger.error(err);
-  		res.sendStatus(500);//internal server error
+  		res.status(500).send(err);//internal server error
   	}else{
   		res.status(200).send(fileName); //send back that everything went ok	
+  		/*notify users of new record of this journey new record*/
   		updateChildren(req.params.journey_id);
   	}
   });
   
 });
+/*add user as a listener to all records and then continue execution path*/
+async function multiDocsUpdate(docs, trip_id, callback){
+	for(var i = 0 ; i < docs.length; i++){
+		if(!docs[i].users.find((entry) => entry === trip_id)){
+			/**user didn't listen to this before**/
+			/*mark user a listener*/
+			docs[i].users.push(trip_id);
+			var ret = await docs[i].save();
+		}	
+	}
+	callback(null, docs);
+}
 
-
+/*get records for user and trip*/
 app.get('/getRecords/', (req, res) => {
+	/*get subscription for this user trip and journey*/
 	Subscription.find({journey_id: req.query.journey_id, trip_id: req.query.trip_id, is_active: true}, (err, docList) => {
 		if(err){
 			res.status(500).send('something went wrong' + err);
 			logger.error( err);
 		}else if(docList.length != 1){
+			/*if either subscription is found more than once or not found, send error*/
 			res.status(500).send('subscription not found or inactive');
 		}
 		/* if user subscribed */
 		else if(docList.length == 1){
-
+			/*get the time of subscription*/
 			let lastTime = docList[0].time;
 			if(req.query.lastTime && req.query.lastTime > lastTime){
 				lastTime = req.query.lastTime;
 			}
+			/*find recording for this journey recorded after the subscription issued*/
 			Recording.find({journey_id: req.query.journey_id, time: {$gt: lastTime}}, (err2, docList2)=>{
-				var newList = JSON.parse(JSON.stringify(docList2));
-				for(var i = 0; i < newList.length ; i++){
-					newList[i].path = uploadPath + getFilePath(docList2[i].journey_id, docList2[i].time);
-					newList[i].time = docList2[i].time;
-				}
+				/*make new instance of recording list*/
+				multiDocsUpdate(docList2, req.query.trip_id,(err, newDocs)=>{
+					if(err){
+						res.status(500).send(err + 'll');
+					}else{
+						var newList = JSON.parse(JSON.stringify(newDocs));
+						for(var i = 0; i < newList.length ; i++){
+							/*add path property to recrods for user to use*/
+							newList[i].path = uploadPath + getFilePath(newDocs[i].journey_id, newDocs[i].time);
+							newList[i].time = newDocs[i].time;
+						}
+						res.status(200).send(newList);		
+					}
+				})
+				
+
 				// console.log(newList);
-				res.status(200).send(newList);
+				
 			})
 		}
 	});
 });
-
+/*finish user trip by setting their subscription to inactive*/
 app.post('/finishTrip', (req, res) => {
 	if(req.body.trip_id && req.body.journey_id){
+		/*find subscription for user, and set it to inactive*/
 		Subscription.updateOne({journey_id: req.body.journey_id, trip_id: req.body.trip_id, is_active: true},
 			{is_active: false},
 			(err, ret)=>{
@@ -147,7 +206,7 @@ app.post('/finishTrip', (req, res) => {
 	}
 });
 
-//app.listen(port, () => console.log(`Example app listening on port http://localhost:${port}!`))
+/*start server by listening on specified port*/
 http.listen( port, () => {
 	var message = `server is running on http://localhost:${port}`;
   logger.log('info',message);
